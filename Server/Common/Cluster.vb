4f3a6e063ea230517536a79eb46a27ec5f5d691b
wow-masters <wow-masters@60427e00-2fe0-45d7-965b-4311af73d861>
2010-06-10 04:44:57 +0000

    Cluster Updates.

--- Server/Common/Cluster.vb
+++ Server/Common/Cluster.vb
@@ -77,9 +77,12 @@ Public Interface IWorld
     <Description("Respond to world server if still alive.")> _
     Function Ping(ByVal Timestamp As Integer) As Integer
 
-    <Description("Tell the cluster about your CPU.")> _
-    Sub ServerInfo(ByRef CPUUsage As Single, ByRef MemoryUsage As ULong)
+    <Description("Tell the cluster about your CPU Usage.")> _
+    Sub ServerInfo(ByRef CPUUsage As Single)
 
+	    <Description("Tell the cluster about your Memory Usage.")> _
+    Sub ServerInfo(ByRef MemoryUsage As ULong)
+	
     <Description("Make world create specific map.")> _
     Sub InstanceCreate(ByVal Map As UInteger)
     <Description("Make world destroy specific map.")> _
